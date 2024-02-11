60231b06d4d3c7250ed1e9fd2e4b5d38a4eb90ba
Micloudtest <145197598+Micloudtest@users.noreply.github.com>
2024-02-11 10:33:46 +0800

    Update twrp_begonia.mk

--- twrp_begonia.mk
+++ twrp_begonia.mk
@@ -8,6 +8,8 @@
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
+
+# Include GSI keys
 $(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
 
 # Inherit from begonia device
